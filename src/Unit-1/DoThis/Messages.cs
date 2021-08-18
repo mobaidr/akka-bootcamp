236b3d2c0db71491ce50e16a2006233f3486b957
Mohammed O Rahman <mohammed_rahman@symbotic.com>
2021-08-18 22:41:53 +0530

    Introduced new Messages and polished the code. Defining and Handling the messages

---
+++ src/Unit-1/DoThis/Messages.cs
@@ -0,0 +1,58 @@
+﻿namespace WinTail
+{
+    public class Messages
+    {
+
+
+        #region Neutral/System Messages
+
+        public class ContinueProcessing
+        {
+
+        }
+
+        #endregion
+
+        #region Success Messages
+
+        public class InputSuccess
+        {
+            public InputSuccess(string reason)
+            {
+                this.Reason = reason;
+            }
+
+            public string Reason { get; private set; }
+        }
+
+        #endregion
+
+        #region Error Messages
+        public class InputError
+        {
+            public InputError(string reason)
+            {
+                Reason = reason;
+            }
+
+            public string Reason { get; private set; }
+        }
+
+        /// <summary>
+        /// User provided blank input.
+        /// </summary>
+        public class NullInputError : InputError
+        {
+            public NullInputError(string reason) : base(reason) { }
+        }
+
+        /// <summary>
+        /// User provided invalid input (currently, input w/ odd # chars)
+        /// </summary>
+        public class ValidationError : InputError
+        {
+            public ValidationError(string reason) : base(reason) { }
+        }
+        #endregion
+    }
+}
